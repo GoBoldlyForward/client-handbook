f86ab8115c26c40d2d1cf230b0c9ec08208eec2f
Jared <jared@lessmade.com>
2018-04-27 15:52:36 -0400

    Mobile styling tweaks

--- main.js
+++ main.js
@@ -2,16 +2,41 @@
 
 (function( $ ){
 
+  /* main nav title magic */
+  var navTitle = $(".navbar");
+      fadeIn = "fade-in";
+      header = $('.large-header').height();
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > header ) {
+      navTitle.addClass(fadeIn);
+    } else {
+      navTitle.removeClass(fadeIn);
+    }
+  });
+
+  /* Secondary nav */
   var volNavigation = $(".chapter-nav");
       sticky = "stickemup";
       header = $('.large-header').height();
 
   $(window).scroll(function() {
-    if( $(this).scrollTop() > header ) {
+    if( $(this).width() > 960 ) {
+      if( $(this).scrollTop() > header ) {
+        volNavigation.addClass(sticky);
+      } else {
+        volNavigation.removeClass(sticky);
+      }
+  }
+    if( $(this).width() < 960 ) { {
+    if($(this).scrollTop() + $(this).height() == $(document).height()) {
       volNavigation.addClass(sticky);
     } else {
       volNavigation.removeClass(sticky);
     }
+   }
+  }
+
   });
 
 })( jQuery );
